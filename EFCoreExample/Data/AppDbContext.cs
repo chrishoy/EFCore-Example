c703dc6906bdd26055cd42e345916cd58187f70a
Chris Hoy <15720741+chrishoy@users.noreply.github.com>
2025-03-17 17:56:19 +0000

    Add project files.

---
+++ EFCoreExample/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCoreExample.Data;
+
+using EFCoreExample.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+}
